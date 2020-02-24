8130be42074dd8e4f0569ae36fb3a91aac4b7e4d
Aleshkin Paul <vinci.aleshkin@gmail.com>
2020-02-24 17:16:47 +0300

    feat: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array === undefined || array.length === 0) ? 0 : array.sort(sortMinToMax)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array === undefined || array.length === 0) ? 0 : array.sort(sortMaxToMin)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array === undefined || array.length === 0) ? 0 : array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+function sortMinToMax(x,y) {
+  if (Number(x) > Number(y)) return 1;
+  if (Number(x) < Number(y)) return -1;
+  if (Number(x) > Number(y)) return 0;
+}
+
+function sortMaxToMin(x,y) {
+  if (Number(x) > Number(y)) return -1;
+  if (Number(x) < Number(y)) return 1;
+  if (Number(x) > Number(y)) return 0;
 }
